3a4d36f59bb2cfa883df1454b3a0252f60380716
FernandesJ20 <fernandes.julia@escolar.ifrn.edu.br>
2025-07-31 17:41:55 +0000

    desafio 7

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,19 @@
+
 import styles from "./Header.module.css";
-import { Link } from "react-router";
 import { ShoppingBasket } from "lucide-react";
+import { Link } from "react-router";
 import { useContext } from "react";
 import { CartContext } from "../service/CartContext";
 
-export function Header() {
 
+export function Header() {
   const { cart } = useContext(CartContext);
-  
   return (
-    <header className={styles.header}>
-      <Link to="/"><h1>TRJ Megastore</h1></Link>
-      <div>
-        <Link to="/cart"><ShoppingBasket size={24} /></Link>
-        <p>
-          Total $: {cart.reduce((total, product) => total + product.price * product.quantity, 0).toFixed(2)}
-        </p>
+    <header className={styles.header1}>
+      <Link to="/" className={styles.title}>julia</Link>
+      <div className={styles.cart}>
+        <Link to="/cart"><ShoppingBasket /></Link>
+        { cart.length === 0 ? <h5></h5> : <p>{cart.length}</p>}
       </div>
     </header>
   );
