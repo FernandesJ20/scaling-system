a9635e9f47be8e100528304502cf2b4b62f211b1
FernandesJ20 <fernandes.julia@escolar.ifrn.edu.br>
2025-08-16 02:56:21 +0000

    desafio final

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,12 @@ import "./styles/global.css";
 import { Header } from "./components/Header";
 import { Routes, Route } from "react-router";
 import { CartProvider } from "./service/CartContext";
-import { Cart } from "./components/Cart";
-import { ProductList } from "./components/ProductList";
+import { Cart } from "./components/etapa2/Cart";
+import { ProductList } from "./components/etapa2/ProductList";
+import { Login } from "./components/etapa2/Login";
+import { Signup } from "./components/etapa2/Signup";
+import { Estoque } from "./components/etapa2/Estoque";
+
 
 export default function App() {
   return (
@@ -15,6 +19,9 @@ export default function App() {
       <Routes>
         <Route path="/" element={<ProductList/>}/>
         <Route path="/cart" element={<Cart/>} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/signup" element={<Signup/>} />
+        <Route path="/stock" element={<Estoque/>} />
       </Routes>
     </CartProvider>
     </>
